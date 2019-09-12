48e3cebe1ae1c6ae9a53e42033de25a0dcc0dd27
terencecistudent <terencelogue100@hotmail.co.uk>
2019-09-12 11:10:06 +0000

    final commit

--- js/script.js
+++ js/script.js
@@ -1,6 +1,4 @@
 $(document).ready(function() {
-
-
  	$("#stream1_btn").on("click", function() {
  		$(".stream1").removeClass('highlight_stream');
 		$(".stream2").removeClass('highlight_stream');
@@ -20,13 +18,25 @@ $(document).ready(function() {
 	  	$(".stream3").addClass('highlight_stream');
 	});
 
-	//removes class makeRed, adds class makeBorder on mouseenter
-	$("button").mouseenter(function(){
+	// removes class makeRed, adds class makeBorder on mouseenter
+	$("button").mouseenter(function() {
 		$(this).removeClass("makeRed").addClass("makeBorder");
 	});
 
-	$("button").mouseleave(function(){
+	$("button").mouseleave(function() {
 		$("button").removeClass("makeBorder").addClass("makeRed");
 	});
 
+	$("button").click(function() {
+		$("p").slideToggle(2000);
+	});
+
+	// hides/shows paragraphs when either button is clicked
+	$("button").click(function() {
+		$("p").hide(2000).show(2000);
+	});
+
+	$("button").click(function(){
+		$("p").fadeIn().fadeOut();
+	});
 });
